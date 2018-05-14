f5208eb48be5019b4a573888d28def312f0d1be7
Sylwuchaa <uzumagi1@gmail.com>
2018-05-14 20:00:46 +0200

    generate your team, click on character :)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <h3>{{title}}</h3>
-    <Character />
+    <div class="col-md-12 d-flex flex-wrap">
+        <Character 
+            v-for="(id, ind3) in inital_ids"
+            :id="id"
+            key="index"
+        />
+    </div>
+ 
   </div>
 </template>
 <script>
     import Character from './components/Character.vue'
+    
     export default {
         name: 'app',
         data() {
             return {
-                title: 'Generate Your Team'
+                title: 'Generate Your Team',
+                inital_ids: [1, 13, 14, 16]
             }
         },
         components: {
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,25 +1,42 @@
 <template>
-  <div class="">
-      {{character}}</div>
+<div class="col-md-6" @click="switchCharacter"> 
+
+
+
+    <div class="character-card">
+        <div class="card-block">
+            <h4 class="card-title">{{character.name}}</h4>
+            <p class="card-text">Height: {{character.height}}cm</p>
+            <p class="card-text">Mass: {{character.mass}}kg</p>
+            <p class="card-text">Hair Color: {{character.hair_color}}</p>
+            <p class="card-text">Eye Color: {{character.eye_color}}</p>
+        </div>
+    </div>
+</div>    
 </template>
 <script>
 export default {
+    props: ['id'],
     data() {
         return {
             character: {}
         }
     },
     methods: {
-        fetchCharacter() {
-                fetch('http://swapi.co/api/people/1', {
+        fetchCharacter(id) { 
+                fetch(`http://swapi.co/api/people/${id}`, {
                     method: 'GET'
                 })
                 .then(response => response.json())
                 .then(json => this.character = json)
-            }
+            },
+        switchCharacter() {
+            let random_id = Math.floor(Math.random() * 83) + 1
+            this.fetchCharacter(random_id)
+        }
     },
     created() {
-        this.fetchCharacter()
+        this.fetchCharacter(this.id)
     }
 }
 </script>
