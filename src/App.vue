0f61b5edc78bf7cd640496f4ab79fa9d19ae17ae
Sylwuchaa <uzumagi1@gmail.com>
2018-05-22 17:22:02 +0200

    planets router done

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,10 @@
 <template>
-  <div id="app">
-    <h3>{{title}}</h3>
-    <h4>
-    <router-link class="link" to="/data/planets">Explore the Planets</router-link> |
-    <router-link class="link" to="/data/people">Meet the Characters</router-link>
-    </h4>
-    <router-view></router-view>
-    <div class="col-md-12 d-flex flex-wrap">
-        <Character 
-            v-for="(id, ind3) in inital_ids"
-            :id="id"
-            key="index"
-        />
-    </div>
- 
-  </div>
+<div id="app">
+  <h3>Starbase Router</h3>
+  <h4>
+    <router-link class="link" to="/data/people">Meet the Characters</router-link> |
+    <router-link class="link" to="/data/planets">Explore the Planets</router-link>
+  </h4>
+  <router-view></router-view>
+</div>
 </template>
-<script>
-    import Character from './components/Character.vue'
-    import Data from './components/Data.vue'
-    export default {
-        name: 'app',
-        data() {
-            return {
-                title: 'Generate Your Team',
-                inital_ids: [1, 13, 14, 15]
-            }
-        },
-        components: {
-          Character,
-          Data  
-        }
-            
-    }
-</script>
